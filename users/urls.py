d766103a22f46b9bcb040ea60cec998bd07bf7a9
Haruna <harunanjie@hotmail.com>
2020-11-11 09:48:19 -0800

    Created Signup view and users template directory

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.index, name='home' )
+    path('', views.index, name='home' ),
+    path('signup', views.UserSignup.as_view(), name='signup'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, HttpResponse
-
+from django.views.generic import CreateView
+from . forms import UserCreateForm
+from . models import User
 # Create your views here.
 
 def index(request):
-    return HttpResponse('hello world')
+    return HttpResponse('hello world!!')
+
+class UserSignup(CreateView):
+    form_class = UserCreateForm
+    model = User
+    template_name = 'users/signup.html'
+    success_url = '/'
